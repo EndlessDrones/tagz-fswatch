32de0f535cc34f40a03b6c4d3faff415aa206eb1
emkor93 <emkor93@gmail.com>
2019-04-12 19:30:07 +0200

    added support for renaming and selecting extensions

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"crypto/sha256"
+	"errors"
+	"fmt"
 	"github.com/fsnotify/fsnotify"
 	"github.com/gabriel-vasile/mimetype"
 	"io"
 	"log"
+	"mime"
 	"os"
 	"path/filepath"
 	"time"
@@ -17,13 +20,14 @@ const (
 	TAGZ_OUT_DIR = "/home/mat/Projects/tagz/tmp/out"
 )
 
-type NewFile struct {
-	name    string
-	ext     string
-	size    int64
-	modTime time.Time
-	mime    string
-	sha256  []byte
+type FileMeta struct {
+	origName  string
+	origExt   string
+	size      int64
+	modTime   time.Time
+	mime      string
+	sha256    []byte
+	sha256Str string
 }
 
 func processInotify(inotifyWatcher *fsnotify.Watcher, notif chan<- string) {
@@ -50,14 +54,16 @@ func processInotify(inotifyWatcher *fsnotify.Watcher, notif chan<- string) {
 	}
 }
 
-func checkIfMovable(newFile <-chan string, movableFile chan<- string) {
+func checkIfMovable(tmpDir string, newFile <-chan string, movableFile chan<- string) {
 	for filePath := range newFile {
-		tgtPath := filepath.Join(TAGZ_TMP_DIR, filepath.Base(filePath))
+		tgtPath := filepath.Join(tmpDir, filepath.Base(filePath))
 		if _, err := os.Stat(tgtPath); err != nil {
 			err := os.Rename(filePath, tgtPath)
 			if err == nil {
 				movableFile <- tgtPath
 			}
+		} else {
+			log.Printf("Ignoring %s because there's same file under %s", filePath, tgtPath)
 		}
 	}
 	close(movableFile)
@@ -77,55 +83,99 @@ func makeWatcher(watchDir string) *fsnotify.Watcher {
 	return watcher
 }
 
-func handleMovableFile(movable <-chan string) {
-	for filePath := range movable {
-		filePathInfo, err := os.Stat(filePath)
+func handleMovableFile(tmpDir string, outDir string, movedFilePaths <-chan string, fileMetas chan<- FileMeta) {
+	for filePath := range movedFilePaths {
+		fileMeta, err := getFileMeta(filePath)
 		if err != nil {
-			log.Panicf("Error on opening file %s : %s", filePath, err)
-		}
-		if filePathInfo.IsDir() {
-			log.Printf("tagz does not support directories (yet): %s", filePath)
+			log.Print(err)
 		} else {
-			log.Printf("Moved file %s", filePath)
-
-			newFile, err := os.Open(filePath)
+			err := moveFromTmpDoTgt(tmpDir, outDir, fileMeta)
 			if err != nil {
-				log.Fatal(err)
+				log.Print(err)
+			} else {
+				fileMetas <- fileMeta
 			}
-			defer newFile.Close()
-
-			buf := make([]byte, 1024*1024)
-			h := sha256.New()
-			if _, err := io.CopyBuffer(h, newFile, buf); err != nil {
-				log.Fatal(err)
-			}
-
-			sha256Sum := h.Sum(nil)
-			_, err = newFile.Seek(0, 0)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			mimeType, ext, err := mimetype.DetectReader(newFile)
-
-			if err != nil {
-				mimeType = ""
-			}
-			n := NewFile{name: filePathInfo.Name(), ext: ext, mime: mimeType, modTime: filePathInfo.ModTime(), size: filePathInfo.Size(), sha256: sha256Sum}
-			log.Printf("New file: %s %d %s %s %x", n.name, n.size, n.modTime, n.mime, n.sha256)
-
 		}
 	}
 }
 
-func main() {
-	log.Printf("Hello, world! Watching: %s and moving to %s", TAGZ_IN_DIR, TAGZ_TMP_DIR)
+func moveFromTmpDoTgt(tmpDir string, outDir string, file FileMeta) error {
+	err := os.Rename(filepath.Join(tmpDir, file.origName), buildFilePath(outDir, file))
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error on moving file %s from %s to %s: %s", file.origName, tmpDir, outDir, err))
+	} else {
+		return nil
+	}
+}
+
+func buildFilePath(outDir string, fileMeta FileMeta) string {
+	// TODO fix behavior on binary .img files!
+	defaultPath := filepath.Join(outDir, fileMeta.sha256Str+"."+fileMeta.origExt)
+	mimeExt, err := mime.ExtensionsByType(fileMeta.mime)
+	if err != nil {
+		return defaultPath
+	}
+	extBasedMime := mime.TypeByExtension(fileMeta.origExt)
+	if extBasedMime == "" || extBasedMime != fileMeta.mime {
+		return filepath.Join(outDir, fileMeta.sha256Str+mimeExt[0])
+	} else {
+		return defaultPath
+	}
+}
+
+func getFileMeta(filePath string) (FileMeta, error) {
+	fileStatInfo, err := os.Stat(filePath)
+	if err != nil {
+		return FileMeta{}, errors.New(fmt.Sprintf("Error on opening file %s : %s", filePath, err))
+	}
+	if fileStatInfo.IsDir() {
+		return FileMeta{}, errors.New(fmt.Sprintf("tagz does not support directories (yet): %s", filePath))
+	} else {
+		newFile, err := os.Open(filePath)
+		if err != nil {
+			return FileMeta{}, errors.New(fmt.Sprintf("Error on opening file %s : %s", filePath, err))
+		}
+		defer newFile.Close()
+
+		buf := make([]byte, 1024*1024)
+		h := sha256.New()
+		if _, err := io.CopyBuffer(h, newFile, buf); err != nil {
+			return FileMeta{}, errors.New(fmt.Sprintf("Error on calculating file hash %s : %s", filePath, err))
+		}
+
+		sha256Sum := h.Sum(nil)
+		_, err = newFile.Seek(0, 0)
+		if err != nil {
+			return FileMeta{}, errors.New(fmt.Sprintf("Error on seeking to the beginning of the file %s : %s", filePath, err))
+		}
+
+		mimeType, ext, err := mimetype.DetectReader(newFile)
+
+		if err != nil {
+			return FileMeta{}, errors.New(fmt.Sprintf("Error on determining file %s MIME type: %s", filePath, err))
+		}
+		n := FileMeta{origName: fileStatInfo.Name(), origExt: ext, mime: mimeType, modTime: fileStatInfo.ModTime(), size: fileStatInfo.Size(), sha256: sha256Sum, sha256Str: fmt.Sprintf("%x", sha256Sum)}
+		return n, nil
+	}
+}
+
+func watchForNewFiles(watcher *fsnotify.Watcher, tmpDir string, outDir string, newFileMeta chan<- FileMeta) {
 	fileWrites := make(chan string)
 	fileNotWritingTo := make(chan string)
+	go processInotify(watcher, fileWrites)
+	go checkIfMovable(tmpDir, fileWrites, fileNotWritingTo)
+	go handleMovableFile(tmpDir, outDir, fileNotWritingTo, newFileMeta)
+}
+
+func main() {
+	log.Printf("Hello, world! Watching: %s and moving to %s", TAGZ_IN_DIR, TAGZ_OUT_DIR)
+	newFileMeta := make(chan FileMeta)
 	watcher := makeWatcher(TAGZ_IN_DIR)
 	defer watcher.Close()
 
-	go processInotify(watcher, fileWrites)
-	go checkIfMovable(fileWrites, fileNotWritingTo)
-	handleMovableFile(fileNotWritingTo)
+	watchForNewFiles(watcher, TAGZ_TMP_DIR, TAGZ_OUT_DIR, newFileMeta)
+	for fm := range newFileMeta {
+		log.Printf("New file: %s %d %s %s %x", fm.origName, fm.size, fm.modTime, fm.mime, fm.sha256)
+	}
+	log.Print("Exiting!")
 }
